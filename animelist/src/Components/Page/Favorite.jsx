d209c773fa577697caaf7041e330e0bd3c1d69c8
Jiraphat <jiraphat.note@gmail.com>
2024-07-27 15:20:28 +0700

    edit fav bg2

--- animelist/src/Components/Page/Favorite.jsx
+++ animelist/src/Components/Page/Favorite.jsx
@@ -35,35 +35,31 @@ const Favorite = () => {
             </div>
 
             <div className="grid grid-cols-5 gap-4 mx-10 max-sm:grid-cols-1 max-md:grid-cols-2 max-lg:grid-cols-3 max-xl:grid-cols-4  fav-bg">
-              {favorites.length > 0 ? (
-                favorites.map((anime) => (
-                  <div className="flex-col " key={anime.id}>
-                    <Card className="bg-white/20">
-                      <div className="flex-col justify-center mx-10 box ">
-                        <div className="h-8">
-                          <p className="text-2xl truncate hover:text-balance text-white sm:text-lg md:text-xl">
-                            {anime.attributes?.titles?.en_jp}
-                          </p>
-                        </div>
-                        <img
-                          src={anime.attributes?.posterImage?.medium}
-                          alt={anime.attributes?.titles?.en}
-                          className="object-cover rounded-xl img-border justify-self-center my-5"
-                          width={270}
-                          onClick={() => setSelectedAnime(anime)}
-                        />
+              {favorites.map((anime) => (
+                <div className="flex-col " key={anime.id}>
+                  <Card className="bg-white/20 ">
+                    <div className="flex-col justify-center mx-10 box ">
+                      <div className="h-8">
+                        <p className="text-2xl truncate hover:text-balance text-white sm:text-lg md:text-xl">
+                          {anime.attributes?.titles?.en_jp}
+                        </p>
                       </div>
-                      <CardFooter className="justify-center absolute bottom-1">
-                        <Button onClick={() => handleDeleteClick(anime.id)}>
-                          X
-                        </Button>
-                      </CardFooter>
-                    </Card>
-                  </div>
-                ))
-              ) : (
-                <p className="text-white mx-10">No favorite anime yet.</p>
-              )}
+                      <img
+                        src={anime.attributes?.posterImage?.medium}
+                        alt={anime.attributes?.titles?.en}
+                        className="object-cover rounded-xl img-border justify-self-center my-5"
+                        width={270}
+                        onClick={() => setSelectedAnime(anime)}
+                      />
+                    </div>
+                    <CardFooter className="justify-center absolute bottom-1">
+                      <Button onClick={() => handleDeleteClick(anime.id)}>
+                        X
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </div>
+              ))}
             </div>
           </div>
         </>
